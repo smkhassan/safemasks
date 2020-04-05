cf5abc2ad4d5d71c64abebb0ab0f607651c68de7
smkhassan <futureco@email.cz>
2020-04-05 14:52:53 +0200

    url parser

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from safe_masks_crawler import alibaba_module
 # flag = True
 # while flag:
 
-alibaba_module.crawler.crawl()
+alibaba_module.crawler.crawl('https://www.alibaba.com/product-detail/5Layers-Multifunctional-Face-Masks-KN95-Respirator_62532136916.html?spm=a2700.galleryofferlist.0.0.125c66d3DbeLJk&s=p')
