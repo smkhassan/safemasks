eef7ba49b7cc2da0e9b67146c97012656a155622
smkhassan <futureco@email.cz>
2020-04-05 14:08:42 +0200

    Event Hub Triger v1

--- event_hub_trigger/__init__.py
+++ event_hub_trigger/__init__.py
@@ -7,6 +7,5 @@ from safe_masks_crawler import alibaba_module
 def main(event: func.EventHubEvent):
     logging.info('Python EventHub trigger processed an event: %s',
                  event.get_body().decode('utf-8'))
-    alibaba_module.crawler.crawl()             
 
 
